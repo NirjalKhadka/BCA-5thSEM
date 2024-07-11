8c39a940e0e0f67d57b0c1e5490ae0646362edae
NirjalKhadka <khadkanirjal779@gmail.com>
2024-07-11 10:54:55 +0545

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using classandobject.cars;
+using System;
+
+namespace classandobject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Model = "bugatti";
+            car1.Color = "Red";
+            //car1.number = 224;tnet.run
+
+            car1.start();
+            Console.WriteLine("Car Details:");
+            car1.details(car1.Model,car1.Color);
+            Console.ReadKey();
+        }
+    }
+}
